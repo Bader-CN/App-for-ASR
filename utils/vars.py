a1410b193990773ac262d2eb9488685bd5b3640a
Bader <zhanghong.personal@outlook.com>
2026-01-06 10:26:38 +0800

    Update vars.py
    Asynchronous initialization model

--- utils/vars.py
+++ utils/vars.py
@@ -1,3 +1,4 @@
+import time
 import torch
 
 from queue import Queue
@@ -13,8 +14,8 @@ from langchain.chat_models import init_chat_model
 from utils.log import logger
 from utils.cfg import cfg
 
-logger.info("Initializing global parameters and models...")
-
+logger.info("Initialize other parameters...")
+#############################################
 # 队列相关
 Audio_Queue = Queue()                                                           # 语音切片队列
 ASR_Result_Queue = Queue()                                                      # ASR 识别结果队列
@@ -37,49 +38,83 @@ LLM_Executor = ThreadPoolExecutor(max_workers=1)                                
 
 # 事件标记
 Task_ASR = Event()                                                              # 设置则意味着开始语言识别, 反之则取消语言识别
-Task_ASR.clear()
 
 # 翻译相关 - 这里必须使用引用变量(非 int/str/None), 否则其它模块导入时只是 "静态快照", 不是 "动态引用"
 Task_LLM = {"TGT_LANG": None, "SYS_Prompt": None}
+#############################################
+logger.success("Finish initialized other parameters.")
 
-# ASR Tokenizer
-ASR_Tokenizer = AutoTokenizer.from_pretrained(cfg.get("ASR", "ASR_Model"))
+
+def init_ASR():
+    """
+    初始化 ASR 的 tokenizer, model, pipeline
+    """
+    logger.info("Initializing ASR tokenizer, model and pipeline...")
+    # ASR Tokenizer
+    ASR_Tokenizer = AutoTokenizer.from_pretrained(cfg.get("ASR", "ASR_Model"))
  
-# ASR Pipeline
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-ASR_Model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    cfg.get("ASR", "ASR_Model"), 
-    dtype=torch_dtype, 
-    # low_cpu_mem_usage=True
-)
-ASR_Model.to(device)
+    # ASR Pipeline
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+    ASR_Model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        cfg.get("ASR", "ASR_Model"), 
+        dtype=torch_dtype, 
+        # low_cpu_mem_usage=True
+    )
+    ASR_Model.to(device)
 
-Processor = AutoProcessor.from_pretrained(cfg.get("ASR", "ASR_Model"))
-# 利用 ASR_Pipeline 来进行推理
-# https://huggingface.co/docs/transformers/v5.0.0rc1/en/main_classes/pipelines#transformers.AutomaticSpeechRecognitionPipeline
-ASR_Pipeline = pipeline(
-    "automatic-speech-recognition",
-    model=ASR_Model,
-    tokenizer=Processor.tokenizer,
-    feature_extractor=Processor.feature_extractor,
-    dtype=torch_dtype,
-    device=device,
-)
+    Processor = AutoProcessor.from_pretrained(cfg.get("ASR", "ASR_Model"))
+    # 利用 ASR_Pipeline 来进行推理
+    # https://huggingface.co/docs/transformers/v5.0.0rc1/en/main_classes/pipelines#transformers.AutomaticSpeechRecognitionPipeline
+    ASR_Pipeline = pipeline(
+        "automatic-speech-recognition",
+        model=ASR_Model,
+        tokenizer=Processor.tokenizer,
+        feature_extractor=Processor.feature_extractor,
+        dtype=torch_dtype,
+        device=device,
+    )
+    logger.success("Finish initialized ASR tokenizer, model and pipeline.")
+    return ASR_Tokenizer, ASR_Model, ASR_Pipeline
+
+# ASR_Tokenizer, ASR_Model, ASR_Pipeline = ASR_Executor.submit(init_ASR).result()
+ASR_Init = ASR_Executor.submit(init_ASR)
+
+
+# LLM Model
+def init_LLM():
+    """
+    初始化 LLM
+    """
+    logger.info("Initializing LLM...")
+    LLM_Model = init_chat_model(
+        model_provider=cfg.get("LLM", "Provider"),
+        model = cfg.get("LLM", "Model_Name"),
+        base_url = cfg.get("LLM", "Base_URL"),
+        max_tokens = cfg.getint("LLM", "MAX_Tokens"),
+        api_key = cfg.get("LLM", "API_Key"),
+    )
+    llm_init = LLM_Model.invoke("ASR translation model has been successfully loaded!")
+    logger.success(f"Finish initialized LLM: {llm_init.response_metadata}")
+    return LLM_Model
+
+# LLM_Model = LLM_Executor.submit(init_LLM).result()
+LLM_Init = LLM_Executor.submit(init_LLM)
 
 # VAD Model
 VAD_Model = load_silero_vad()
 VAD_Model.to("cpu")
 
-# LLM Model
-LLM_Model = init_chat_model(
-    model_provider=cfg.get("LLM", "Provider"),
-    model = cfg.get("LLM", "Model_Name"),
-    base_url = cfg.get("LLM", "Base_URL"),
-    max_tokens = cfg.getint("LLM", "MAX_Tokens"),
-    api_key = cfg.get("LLM", "API_Key"),
-)
-llm_init = LLM_Model.invoke("ASR translation model has been successfully loaded!")
-logger.info(f"{llm_init.response_metadata}")
-
-logger.success("Finish initialized global parameters and models.")
+# 异步初始化结果
+init_llm = True
+init_asr = True
+while init_llm is True or init_asr is True:
+    if LLM_Init.done():
+        LLM_Model = LLM_Init.result()
+        init_llm = False
+    
+    if ASR_Init.done():
+        ASR_Tokenizer, ASR_Model, ASR_Pipeline = ASR_Init.result()
+        init_asr = False
+    
+    time.sleep(0.2)
