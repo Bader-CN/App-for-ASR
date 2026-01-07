e4644b0325572d797110e43938c825c5d920ea68
Bader <zhanghong.personal@outlook.com>
2026-01-07 09:57:49 +0800

    Update Real-time ASR.py
    解决程序无法正常关闭的问题

--- Real-time ASR.py
+++ Real-time ASR.py
@@ -1,12 +1,17 @@
 import webview
 from app import demo as gradio_app
 
+# 设定一个关闭函数
+def on_closed():
+    gradio_app.close()
+    print('pywebview window is closed')
+
 # If set to True, the gradio app will not block and the gradio server will terminate as soon as the script finishes.
 # https://www.gradio.app/4.44.1/docs/gradio/blocks
 gradio_app.launch(prevent_thread_lock=True)
 
 # https://pywebview.flowrl.com/api/#webview-create-window
-webview.create_window(
+window = webview.create_window(
     title="Real-time ASR", 
     url=gradio_app.local_url,
     width=1300,
@@ -16,4 +21,6 @@ webview.create_window(
     # 始终位于其它窗口之上
     on_top=False,
     )
+# https://pywebview.flowrl.com/examples/events.html
+window.events.closed += on_closed
 webview.start()
